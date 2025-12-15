62b12df9d23678b335f0296817987361a74d1262
James Smith <js5@sanger.ac.uk>
2025-12-15 12:57:54 +0000

    Enhance comments and clean up range logic
    Refactor range processing and improve comments

--- day2.pl
+++ day2.pl
@@ -6,6 +6,21 @@ use feature qw(say signatures);
 use Data::Dumper qw(Dumper);
 use Time::HiRes qw(time);
 
+## See: https://adventofcode.com/2025/day/2
+
+## A bit of a cheat here - manually create the list of multipliers which
+## correspond to numbers that are of the form ^(.*){2+}$
+## So 1 means that the numbe is of the form aaaaaaaaaa
+##    2 means that it is the form           ababababab
+##    etc
+## The 2nd number is the multiplier for that length of string -
+##    so for 4 - 101 gives you 1010 1111 1212 ... 1919 2020 ....
+## 3rd number just indicates if the pattern is repeated twice
+## 4th number is to avoid caching - so e.g. for length 6
+##   We have length 2 & length 3 loops ababab & abcabc... In both
+##   cases we count aaaaaa, and so we count aaaaaa twice so we
+##   have to "uncount" these once... same for the case for
+##   length 10.
 my @l = (
    [],
    [],
@@ -23,27 +38,33 @@ my @l = (
      [ 5,     100001, 1,  1 ],
      [ 1, 1111111111, 0, -1 ] ] # 10
 );
+
 my( $t0, $t1, $t2 ) = ( time, 0, 0 );
 my $fn = $0 =~ s{([^/]+\.)pl$}{ 'input/'.$1.(@ARGV ? 'test' : 'txt') }er;
 die qq(Missing file "$fn"\n) unless -e $fn;
 
+## read in data of the form - 11-22,95-115 - split ranges which have different
+## lengths e.g. 95-115 -> 95-99 & 100->115.. As how we approach this depends
+## on length...
 open my $fh, '<', $fn;
 my $C;
 for( map { [ split /-/ ] } <$fh>=~ m{(\d+-\d+)}g ) {
   my( $s, $e ) = @{$_};
   while( length $e > length $s ) {
-    _r( $s, -1 + 10**(length $s) );
-    $s = 10 ** length $s;
+    _r( $s, -1 + 10**(length $s) ); ## Process range
+    $s = 10 ** length $s;           ## update start
   }
-  _r( $s, $e );
+  _r( $s, $e ); ## process whats left...
 }
 close $fh;
 
 sub _r( $s, $e ) {
-  my %seen;
   for my $ch ( @{$l[ length $s ]} ) {
     my($sl, $mul, $f, $m) = @{$ch};
-    my $x = (substr $s,0,$sl) * $mul;
+    my $x = ( substr $s, 0, $sl ) * $mul;
+    ## Loop through until we pass the "$e" value, adding up the counts
+    ## see flags above (obviously the start of the loop may be less than
+    ## start of the interval so skip counting those values...
     ( $x >= $s ) && ( $t1 += $x*$f, $t2 += $m*$x ), $x += $mul while $x <= $e
   }
 }
