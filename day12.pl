a04f0505f720258374b0a993de8e52a0835686e7
James Smith <js5@sanger.ac.uk>
2025-12-17 18:16:21 +0000

    Refactor comments and improve readability in day12.pl

--- day12.pl
+++ day12.pl
@@ -10,12 +10,18 @@ my( $t0, $t1, $t2 ) = ( time, 0, 0 );
 my $fn = $0 =~ s{([^/]+\.)pl$}{ 'input/'.$1.(@ARGV ? 'test' : 'txt') }er;
 die qq(Missing file "$fn"\n) unless -e $fn;
 
+## This is a trick question because the search space is so vast it
+## is unsolvable in most cases - but we can observe when a solution
+## is always possible (trivial solution of placing them in any order
+## in a grid pattern OR the solution is impossible - there are more
+## "dots" of parcel than dots on the grid...
+
 if( $fn =~ m{\.test} ) {
   ## We cheat in the test case - there are non-trivial solutions...
   ## So we know the answer is 2.....
   $t1 = 2;
 } else {
-## File format....
+  ## File format....
 =cut
   0:
   #.#
@@ -38,37 +44,35 @@ if( $fn =~ m{\.test} ) {
     ( 0, 0, 0, 0 );
   open my $fh, '<', $fn;
   while(<$fh>) {
-    if( m{[#.]} ) { ## This is part of one of the packages;
-                    ## We just need to count the number of ...                    
-      $w[-1] += tr/#/#/
+    if( m{[#.]} ) {     ## This is part of one of the packages;
+      $w[-1] += tr/#/#/ ## We just need to count the number of ...                    
     } elsif( !m{\S} ) { ## Gap between packages, so initialize
-                        ## the next one...
-      push @w, 0
+      push @w, 0        ## the next one...
     } elsif( m{^(\d+)x(\d+): (.*)} ) {
-      ## Finally this is one of the grids we need to check,
-      ## The first paire of digits is the height & width,
-      ## So we compute the number of whole squares ($s)
-      ## and number of "dots" ($d) AND we then get the numbers
-      ## of each type of present....
+        ## Finally this is one of the grids we need to check,
+        ## The first paire of digits is the height & width,
+        ## So we compute the number of whole squares ($s)
+        ## and number of "dots" ($d) AND we then get the numbers
+        ## of each type of present....
       my ( $s,                    $d,    @counts )
        = ( int($1/3) * int($2/3), $1*$2, split /\s+/, $3 );
-      ## We do things - first we check the number of "dots" of
-      ## puzzles against $d & the number of puzzles against $s
-      ## (the trivial pack)
-      ## If $s >= number of parcels you can do the trival pack
-      ## If $d <  number of "dots" we know there IS no solution
-      ## So we are left with cases where $s < #parcels & $d > #dots
+        ## We do things - first we check the number of "dots" of
+        ## puzzles against $d & the number of puzzles against $s
+        ## (the trivial pack)
+        ## If $s >= number of parcels you can do the trival pack
+        ## If $d <  number of "dots" we know there IS no solution
+        ## So we are left with cases where $s < #parcels & $d > #dots
       $s -=          $counts[$_],
       $d -= $w[$_] * $counts[$_] for 0 .. $#counts;
-      ## We store these in the 3 elements of @x;
+        ## We store these in the 3 elements of @x;
         $s >= 0 ? $always++
       : $d <  0 ? $impossible++
       :           $possible++
     }
   }
   close $fh;
-  ## In our example (at least) $x[1] is "0" there are no non-trivial
-  ## solutions in the real data.... SO...
+    ## In our example (at least) $x[1] is "0" there are no non-trivial
+    ## solutions in the real data.... SO...
   warn "$possible - not trivial solutions\n" if $possible;
   $t1 = $always
 }
