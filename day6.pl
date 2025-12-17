16f548876675b12ffacf95be43ef88f165b55271
James Smith <js5@sanger.ac.uk>
2025-12-17 22:59:05 +0000

    Refactor code for readability and formatting

--- day6.pl
+++ day6.pl
@@ -10,22 +10,25 @@ my( $t0, $t1, $t2 ) = ( time, 0, 0 );
 my $fn = $0 =~ s{([^/]+\.)pl$}{ 'input/'.$1.(@ARGV ? 'test' : 'txt') }er;
 die qq(Missing file "$fn"\n) unless -e $fn;
 
-my( @cl,@vs,@ch,$c);
+my( @cl, @vs, @ch, $c );
 
 open my $fh, '<', $fn;
 while(<$fh>) {
   chomp;
   if( m{\d} ) {
-    $c = 0; push @{$cl[ $c++]}, $_                   for split;
+    $c = 0; push @{ $cl[ $c++ ] }, $_                for split;
     $c = 0; ( $_ eq ' ' ) || ( $vs[$c] .= $_ ), $c++ for split //;
     next;
   }
   $_ ? push( @{$ch[-1]}, $_ ) : ( push @ch, [] ) for 0, @vs;
-  $t1 += e( $_, @{ shift @cl }), $t2 += e( $_, @{ shift @ch   }) for split
+  $t1 += e( $_, @{ shift @cl } ),
+  $t2 += e( $_, @{ shift @ch } ) for split
 }
 close $fh;
 
 sub e( $op, $v, @x ) {
+  ## If OP is + returns the sum of numbers
+  ## If OP is * returns the product of the numbers
   if( $op eq '+' ) { $v += $_ for @x } else { $v *= $_ for @x }
   $v
 }
